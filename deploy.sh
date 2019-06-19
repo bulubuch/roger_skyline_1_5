a7b6e92b2e10751cd83f8eac7489529f2a5b6d45
bulu <bulubuch@gmail.com>
2019-06-19 22:08:09 +0200

    Modified deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set echo off
 # Variables definitions
-TOOLS="exim4 exim4-config fail2ban ssh iptables iptables-persistent sudo crontab git"
 GIT_REPO="https://github.com/bulubuch/roger_skyline_1_5.git"
 
 # Installing softwares
 echo "Roger Skyline 1.5 automated deployment"
-apt-get update -y
-apt-get upgrade -y
+apt-get -y update
+apt-get -y upgrade
 echo "Installing packets used for project..."
-apt-get install $TOOLS -y
+cat /42/config/package.list | xargs apt-get -y install
 
 # Downloading scripts and config files
 echo "Downloading scripts and config files"
@@ -19,8 +18,7 @@ rm -Rf /42/scripts
 
 # Applying config
 echo "Applying config"
-sh /42/firewall
-sed -i 's/dhcp/static
-	address 10.13.0.151/g' /etc/network/interfaces
+sh /42/firewall.sh
+sed -i 's/dhcp/static\n     address 10.13.0.151/g' /etc/network/interfaces
 echo "00 4 * * 0 /42/update.sh" >> /etc/crontab
 echo "00 0 * * * /42/cronCheck.sh" >> /etc/crontab
