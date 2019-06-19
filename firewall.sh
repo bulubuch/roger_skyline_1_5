7584a041158e08df55947763477dcb7dc6e21421
bulu <bulubuch@gmail.com>
2019-06-19 23:33:25 +0200

    Updated firewall

--- firewall.sh
+++ firewall.sh
@@ -9,10 +9,14 @@ iptables -P FORWARD DROP
 # Allow unlimited traffic on loopback
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
- 
+# Allow apt-get
+iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 80 -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 53 -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -p udp --dport 53 -m state --state NEW -j ACCEPTiptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 # Allow incoming ssh only
-iptables -A INPUT -p tcp --dport 4242 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp -s $SERVER_IP -d 0/0 --sport 22 --dport 513:65535 -m state --state ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 4242 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 4242 -m state --state NEW -j ACCEPT
 # make sure nothing comes or goes out of this box
 iptables -A INPUT -j DROP
 iptables -A OUTPUT -j DROP
